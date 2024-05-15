a0ab48205a79e19a791bb417a9a586e8714cdd26
Max Wilkinson <mjswilkinson@outlook.com>
2024-05-15 15:47:35 +0100

    getting email, saving to shared prefs and adding formatting to profile.dart.

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -8,33 +8,33 @@ import 'package:velocity_x/velocity_x.dart';
 import 'package:http/http.dart' as http;
 import 'config.dart';
 
-class SignInPage extends StatefulWidget {
+class SignInPage extends StatefulWidget { // Sign-in page
   @override
   _SignInPageState createState() => _SignInPageState();
 }
 
-class _SignInPageState extends State<SignInPage> {
+class _SignInPageState extends State<SignInPage> { // State class for the sign-in page
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController usernameController = TextEditingController();
   bool _isNotValidate = false;
   late SharedPreferences prefs;
 
-  @override
-  void initState() {
+  @override 
+  void initState() { // Initialize the state
     super.initState();
     initSharedPref();
   }
 
-  void initSharedPref() async {
+  void initSharedPref() async { // Initialize shared preferences
     prefs = await SharedPreferences.getInstance();
   }
 
-  void loginUser() async {
+  void loginUser() async { // Function to log in the user
     if (emailController.text.isNotEmpty &&
         passwordController.text.isNotEmpty &&
-        usernameController.text.isNotEmpty) {
-      var reqBody = {
+        usernameController.text.isNotEmpty) { // Check if the email, password, and username are not empty
+      var reqBody = { // Request body with the user details
         "username": usernameController.text,
         "email": emailController.text,
         "password": passwordController.text
@@ -47,24 +47,27 @@ class _SignInPageState extends State<SignInPage> {
       );
 
       var jsonResponse = jsonDecode(response.body);
-      if (jsonResponse['status'] != null && jsonResponse['status']) {
-        var myToken = jsonResponse['token'];
-        var username = jsonResponse['username'];
-        await prefs.setString('token', myToken);
-        await prefs.setString('username', usernameController.text);
-        Navigator.push(
+      if (jsonResponse['status'] != null && jsonResponse['status']) { // Check if the response status is true
+        var myToken = jsonResponse['token']; // Get the token from the response
+        await prefs.setString('token', myToken); // Save the token to shared preferences
+        await prefs.setString('username', usernameController.text); // Save the username to shared preferences
+        print('Email: ${emailController.text}'); // Add this line to check the email value before saving
+        await prefs.setString('email', emailController.text); // Save the email to shared preferences
+        print('Email saved to prefs: ${prefs.getString('email')}'); // Add this line to check if the email is successfully saved
+
+        Navigator.push( // Navigate to the dashboard page
             context, MaterialPageRoute(builder: (context) => Dashboard(token: myToken)));
       } else {
-        showDialog(
+        showDialog( // Show an error dialog if the login details are invalid
           context: context,
           builder: (BuildContext context) {
-            return AlertDialog(
+            return AlertDialog( // Alert dialog
               title: Text("Error: Invalid Details"),
               content: Text("Please enter the correct login details.\nCheck your username, email, and password."),
               actions: [
                 TextButton(
                   onPressed: () {
-                    Navigator.of(context).pop();
+                    Navigator.of(context).pop(); // Close the dialog
                   },
                   child: Text("Close"),
                 ),
@@ -74,13 +77,13 @@ class _SignInPageState extends State<SignInPage> {
         );
       }
     } else {
-      setState(() {
+      setState(() { // Set the state to show an error if the details are not entered
         _isNotValidate = true;
         showDialog(
           context: context,
           builder: (BuildContext context) {
             return AlertDialog(
-              title: Text("Error: No Details Entered"),
+              title: Text("Error: No Details Entered"), // Alert dialog title
               content: Text("Please enter your login details"),
               actions: [
                 TextButton(
@@ -98,14 +101,14 @@ class _SignInPageState extends State<SignInPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return SafeArea(
+  Widget build(BuildContext context) { // Build the UI
+    return SafeArea( 
       child: Scaffold(
-        body: Container(
+        body: Container( 
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: NetworkImage('https://gifdb.com/images/high/clicking-box-to-open-duck-inside-46iyikvod29znuwl.gif'), // Replace 'YOUR_IMAGE_URL_HERE' with your actual image URL
-              fit: BoxFit.cover, // Adjust the fit as per your requirement
+              image: NetworkImage('https://gifdb.com/images/high/clicking-box-to-open-duck-inside-46iyikvod29znuwl.gif'), // background gif
+              fit: BoxFit.cover, // Adjust the fit as per
             ),
           ),
           child: Center(
@@ -114,7 +117,7 @@ class _SignInPageState extends State<SignInPage> {
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.6), // Set the container color
+                    color: Colors.white.withOpacity(0.6), // Set the container color + opacity
                     borderRadius: BorderRadius.circular(10),
                     
                   ),
@@ -124,7 +127,7 @@ class _SignInPageState extends State<SignInPage> {
                     children: <Widget>[
                       HeightBox(10),
                       "Email Sign-In".text.size(22).color(Colors.black).make(),
-                      TextField(
+                      TextField( // Text field for the username
                         controller: usernameController,
                         keyboardType: TextInputType.text,
                         decoration: InputDecoration(
@@ -135,7 +138,7 @@ class _SignInPageState extends State<SignInPage> {
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.all(Radius.circular(10.0)))),
                       ).p4().px24(),
-                      TextField(
+                      TextField( // Text field for the email
                         controller: emailController,
                         keyboardType: TextInputType.text,
                         decoration: InputDecoration(
@@ -146,7 +149,7 @@ class _SignInPageState extends State<SignInPage> {
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.all(Radius.circular(10.0)))),
                       ).p4().px24(),
-                      TextField(
+                      TextField( // Text field for the password
                         controller: passwordController,
                         keyboardType: TextInputType.text,
                         decoration: InputDecoration(
@@ -157,11 +160,11 @@ class _SignInPageState extends State<SignInPage> {
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.all(Radius.circular(10.0)))),
                       ).p4().px24(),
-                      GestureDetector(
+                      GestureDetector( // Gesture detector for the login button
                         onTap: (){
                           loginUser();
                         },
-                        child: HStack([
+                        child: HStack([ // Horizontal stack for the login button
                           VxBox(child: "LogIn".text.black.makeCentered().p16()).blue400.roundedLg.make(),
                         ]),
                       ),
@@ -172,11 +175,11 @@ class _SignInPageState extends State<SignInPage> {
             ),
           ),
         ),
-        bottomNavigationBar: GestureDetector(
+        bottomNavigationBar: GestureDetector( // Gesture detector for the sign-up button
           onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Registration()));
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>Registration())); // Navigate to the registration page
           },
-          child: Container(
+          child: Container( // Container for the sign-up button
               height: 25,
               color: Colors.lightBlue,
               child: Center(child: "Create a new Account..! Sign Up".text.white.makeCentered())),
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -7,30 +8,33 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  late Future<String?> _usernameFuture; // Future to hold the username
+  late Future<Map<String, String>> _usernameFuture; // Update the type to match the return type of _initializePrefs()
 
   @override
   void initState() {
     super.initState();
-    _usernameFuture = _initializePrefs(); // Initialize SharedPreferences and retrieve the username
+    _usernameFuture = _initializePrefs(); // Assign the Future<Map<String, String>> to _usernameFuture
   }
 
   // Function to initialize SharedPreferences and retrieve the username
-  Future<String?> _initializePrefs() async {
+  Future<Map<String, String>> _initializePrefs() async { // Change the return type to Future<Map<String, String>>
     SharedPreferences prefs = await SharedPreferences.getInstance(); // Get SharedPreferences instance
-    return prefs.getString('username'); // Retrieve the username
+    String? username = prefs.getString('username'); // Retrieve the username from SharedPreferences
+    String? email = prefs.getString('email'); // Retrieve the email from SharedPreferences
+    return {'username': username ?? '', 'email': email ?? ''}; // Return both username and email
   }
 
   // Function to log out
   Future<void> _logout() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove('username'); // Remove username from SharedPreferences
+    await prefs.remove('email'); // Remove email from SharedPreferences
     await prefs.remove('token'); // Remove token from SharedPreferences
     Navigator.pushReplacementNamed(context, '/login'); // Navigate to login page
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { // Update the UI to display user
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
@@ -41,27 +45,81 @@ class _ProfileState extends State<Profile> {
           ),
         ],
       ),
-      body: FutureBuilder<String?>(
-        future: _usernameFuture, // Use the Future holding the username
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator()); // Display a loading indicator while waiting for SharedPreferences initialization
-          } else {
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}')); // Display an error message if SharedPreferences initialization fails
-            } else {
-              String? username = snapshot.data; // Retrieve the username from the snapshot
-              return Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Username: ${username ?? "Unknown"}'), // Display the username or a default value if it's null
-                  ),
-                ],
-              );
-            }
-          }
-        },
+      body: Column(
+        children: [
+          Center(
+            child: FutureBuilder<Map<String, String>>( // Update FutureBuilder to use Map<String, String>
+              future: _usernameFuture, // Use the Future holding the username
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator()); // Display a loading indicator while waiting for SharedPreferences initialization
+                } else {
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}')); // Display an error message if SharedPreferences initialization fails
+                  } else {
+                    String? username = snapshot.data?['username']; // Retrieve the username from the snapshot map
+                    String? email = snapshot.data?['email']; // Retrieve the email from the snapshot map
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 28,
+                          child: Text(
+                            username?.substring(0, 1).capitalized ?? '', // Display the first letter of the username as the avatar
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(height: 6),
+                        Text(
+                          username ?? 'Unknown', // Display the username
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Welcome to your profile!',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 30,),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                  child: Column(
+                                  children: [
+                                    Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text('Username:'),
+                                      Text(username ?? 'Unknown'),
+                                    ],
+                                    ),
+                                    Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text('Email:'),
+                                      Text(email ?? 'Unknown'),
+                                    ],
+                                    ),
+                                  ],
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
